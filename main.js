333319ec983f897eed3af13f6920ec558e453e61
Elshad <118747071+elsad159@users.noreply.github.com>
2025-06-19 15:46:24 +0400

    not2

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
-let start = 500
-let end = 300
+let a = "78987"
 
-for (let index = start; index > end; index--) {
-    console.log(index);
+console.log(a.split("").join(""));
+
+function isPolindrome(a) {
+    if (a.split("").join("") == a.split("").join("")) {
+        console.log(true);
+    } else {
+        console.log(false);
+
+    }
 }
+
+isPolindrome(a)
